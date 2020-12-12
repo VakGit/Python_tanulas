64533f7110f106b41d02a10d3231c9d643ea759d
VakGit <vkopen3@gmail.com>
2020-12-13 00:29:09 +0100

    Update 2_essentials_python.py

--- 2_PYTHON_essentials/2_essentials_python.py
+++ 2_PYTHON_essentials/2_essentials_python.py
@@ -79,5 +79,146 @@ print(type(e))
 végtelen értéke lehet a változónak,  csak a memória mérete szab határt
 """
 
-szam=pow(2,4000)#pow a hatványozás
+szam=pow(2,4)#pow a hatványozás
+#szam=pow(2,4000) ez már nagyon nagy szám lenne
 print(szam)
+
+"""
+-----------------------------------------------------------------------
+***3_STRING***
+-----------------------------------------------------------------------
+
+___**SLICING**____ 
+    - KIIRATAS: 
+               egy tömbrész részhalmazát adja vissza ->>
+              
+              szoveg[kezdőpozició : zaropozicio+1 : lépésköz] 
+  
+A string felfoghagtó egy tömbként , amelynek azonosítói vannak(indexei) pozitiv és negativ index
+pld:
+"A L M A"
+ 
+  0 1 2 3  -> 0-től a végéig 3
+ 
+ -4-3-2-1 -> -1-től az elejéig -4
+
+"""
+#MINTA A SLICING-RA 
+#a minta szöveg
+szoveg="Darabolás!"
+
+#teljes változó kiirása nem slicing változóként
+print (szoveg)
+
+#teljes valtozo kiirasa slicing változóként  az elejétől a végéig , 
+#ha nem adjuk meg a végső értéket akkor végig megy a szövegen
+#alapértelmezet lépésköz 1 
+print(szoveg[0::1])
+print(szoveg[0::])
+print(szoveg[-len(szoveg): :]) #a hosszát a szövegnek meghatároztam, majd negatív indexként megadtam és 1-et hozzáadva ki lett írva a szöveg
+
+#a végétől az elejéig, figyelj hogy -1 a lépésköz ilyenkor 
+print(szoveg[-1::-1])
+print(szoveg[::-1]) #ha -1 a lépésköz úgy felttelezi, hogy a végéről visszafelé haladsz , nem kell a többi paraméter
+
+#az első három betű kiíratása !! 0-val kezd és a 3. indexet már nem írja ki 
+print(szoveg[0:3:])
+print(szoveg[:3:])
+
+#az utolsó három betű kiíratása visszafelé
+print(szoveg[-1:-4:-1])
+#az utolsó három betű kiíratása normál
+print(szoveg[len(szoveg)-3::])
+print(szoveg[-3::])
+
+#minden második betű kiírása
+print(szoveg[1::2])
+
+#az ötödik betű kiírása
+print(szoveg[4])
+print(szoveg[-len(szoveg)+4]) #a pozíció -hossz-normálpozício
+
+"""Rövid program írása slicing bemutatására
+-------------------------------------------
+
+-------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------
+MEGJEGYZÉS: A megkezdett kódrészlet kigészítésa TAB billenbtyű leütésének hatására, ennek a BEÁLLÍTÁSA 
+1. lépés Beállítások 
+2. lépés OPEN SETTING (JSON)
+3. lépés beillesztés a paraméterekhez ["editor.snippetSuggestions": "top"] beállítást
+------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------
+
+Cél:
+...............
+PALINDROM PÉLDA 
+...............
+...............
+oda-vissza megegyezik e a szó 
+pld.
+görög ua. oda-vissza olvasva
+"""
+szoveg="agörög"
+mirror=szoveg[::-1]
+#print(mirror)
+if szoveg==mirror:
+  print(f"::{szoveg}:: szó egy palindrom.")
+else:
+  print(f"::{szoveg}:: szó NEM palindrom.")
+
+
+"""--------------------------------------------------------------------
+FIGYELEM , VIGYÁZAT!!!!!
+A STRING IMMUTABLE !!!!!
+----------------------------------------------------------------------
+A STRING minden értékadásnál új memória címet kap,
+ a stringen végrehajtott műveletek függvények (pld. szoveg.replace())használata esetén ,
+ automatikusan nem íródnak át az változók !!!!
+
+PÉLDÁK:
+"""
+#ÚJ MEMÓRIA CÍM bemutatása
+#változó id-nek bemutatása
+szoveg="alma_1"
+print(f"{szoveg}-> szoveg id:{id(szoveg)} ")
+#változó id-nek bemutatása
+szoveg="alma_2"
+print(f"{szoveg}-> szoveg id:{id(szoveg)} ")
+#látható hogy két különböző hely a memóriában 
+
+
+#SZÖVEGEN FÜGGVÉNNYEL TÖRTÉNŐ VÁLTOZTATÁS HELYES MÓDJA 
+#a szöveg függvénnyel történő manipulása, a palindrom példán keresztül
+#alaptézis
+#---------
+szoveg="indul a görög aludni" #a betűk tekintetében palindrom egyébként nem 
+mirror=szoveg[::-1]
+#print(mirror)
+if szoveg==mirror:
+  print(f"::{szoveg}::  egy palindrom.")
+else:
+  print(f"::{szoveg}::  NEM palindrom.")
+#azaz a szóközök miatt nem az
+
+#módosítása az alapfeltételezésnek 
+#ha nem vesszük figyelembe a szóközöket, akkor palindrom a szöveg 
+szoveg="indul a görög aludni" 
+#kell egy függvény amely kiveszi a SZÖVEGBŐL a szóközöket
+szoveg.replace(" ", "") #.replace(mit,mire,hányszor)
+#nézzük mi lesz
+print(f"::{szoveg}:: ez még az eredeti szöveg!?")#nem lesz jó mert még az eredetiértéket írja ki 
+#mivel a string IMMUTABLE az eredeti változó id. a régi értékre mutat; a változások új helyre kerültek!!!
+#javítsunk !!!!
+#NAGYON FONTOS A VÁLTOZÓK MANIPULÁSA CSAK AZ EREDETI VÁLTOZÓBA BEÍRVA LÉPNEK ÉLETBE
+#LASD
+szoveg=szoveg.replace(" ","")
+print(f"::{szoveg}:: a szöveg így már jó:)")
+mirror=szoveg[::-1]
+#print(mirror)
+if szoveg==mirror:
+  print(f"::{szoveg}::  egy palindrom.")
+else:
+  print(f"::{szoveg}::  NEM palindrom.")
+
+#NAGYON VIGYÁZNI ÍGY NAGYOT LEHET HIBÁZNI C#ban C++ban EZ NEM LENNE HIBA
